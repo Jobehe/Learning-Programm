5508f5804c562dbe5739a11ed86d933331e041ae
Holger Hees <h.hees@esser.local>
2020-09-14 12:50:15 +0200

    ok

---
+++ Training/Pi_Visual.py
@@ -0,0 +1,26 @@
+import turtle as tu
+
+lines = 100_000
+
+
+with open("pi.txt", "r") as f:
+    pi = f.read()
+
+tu.mode('logo')
+tu.tracer(False)
+tu.screensize(3000, 3000, 'black')
+tu.colormode(255)
+
+for n in range(lines):
+    color = int(n/(lines/255))
+    tu.pencolor(255, 255-color, color)
+    zahl = int(pi[n])
+    rotation = zahl * 36
+    tu.setheading(rotation)
+    tu.forward(5)
+    if n % 10_000 == 0:
+        tu.update()
+
+tu.getcanvas().postscript(file='PI_Picture.ps')
+
+tu.done()
--- import time.py
+++
@@ -1,6 +0,0 @@
-
-
-print ("Hallo Welt")
-
-
-' Das ist ein Test'
